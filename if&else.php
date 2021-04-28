f3f61076afe9864b28b9df722f1c4ed8cebbcd9b
renpk <rendykolot21@gmail.com>
2021-04-28 17:33:58 +0700

    contoh contoh PHP

---
+++ if&else.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Belajar PHP part 4</title>
+</head>
+<body>
+
+	<h1>Logika di PHP</h1>
+
+	<?php 
+				// Tipe data bolean
+		// true [benar] / false [salah]
+	// $hasil  = true;
+	// $hasil2 = false;
+
+				// if dan else
+
+	// $password1  = '123';
+	// $password2 = '1234';
+	// 	if( $password1 == '123' ){
+	// 		echo "Selamat anda berhasil masuk!";
+	// 	}else{
+	// 		echo "gagal! passwordnya salah";
+	// 	}
+
+echo "<br>";
+
+	// for ($a = 1; $a <=10; $a++) {
+	// 		// echo $a . "<br>";
+	// 	if($a == 4 || $a == 8) {
+	// 		echo $a;
+		
+	// 	}
+		
+	// }
+
+	$c = 2;
+	$d = 4;
+
+	if ( ($c == 3 && $d == 4) ||  $c == 2) {
+		echo "SMK Antartika";
+	}else {
+		echo "Bukan SMK Antartika";
+	}
+
+
+
+
+	 ?>
+
+</body>
+</html>
